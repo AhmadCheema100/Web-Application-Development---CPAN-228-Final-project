7e9c404ad1c4c1254e82e8023a4c9dafc4f08dc3
Solomon Ozor <n01668239@humber.ca>
2026-03-15 18:13:05 -0400

    Implement H2 database integration, created data.sql and modified application.properties where necessary for Deliverable 1

---
+++ src/main/resources/data.sql
@@ -0,0 +1,8 @@
+INSERT INTO assignment (title, course_name, due_date, priority, status, description, created_at)
+VALUES ('Project Deliverable 1', 'CPAN 228', '2026-03-15', 'HIGH', 'COMPLETED', 'Web Front-End & Database Integration', CURRENT_TIMESTAMP);
+
+INSERT INTO assignment (title, course_name, due_date, priority, status, description, created_at)
+VALUES ('Project Deliverable 2', 'CPAN 228', '2026-03-29', 'MEDIUM', 'IN_PROGRESS', 'Security and User Management', CURRENT_TIMESTAMP);
+
+INSERT INTO assignment (title, course_name, due_date, priority, status, description, created_at)
+VALUES ('Project Deliverable 3: Final Project', 'CPAN 228', '2026-04-16', 'LOW', 'NOT_STARTED', 'Microservices, REST APIs & DevOps (incl. presentation)', CURRENT_TIMESTAMP);
